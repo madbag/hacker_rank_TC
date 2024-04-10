5ecfd85224fbf88f285c21dd1f337132006edaf5
Madhushree B <boylemadhushree@gmail.com>
2024-04-10 15:50:18 +0200

    binary number find

---
+++ day10.js
@@ -0,0 +1,28 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+     console.log(n.toString(2).split('0').map(s => s.length).sort((a, b) => b - a)[0])                  
+}
